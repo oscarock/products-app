2c5bb851d33af10648ee9301acddc423262a3cd4
Oscar Diaz <oscar.diaz@bitsamericas.com>
2021-03-05 17:35:51 -0500

    create test seed product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        DB::table('products')->insert([
+            'name' => 'camisa',
+            'description' => 'camisa manga larga arturo calle',
+            'photo' => 'https://arturocalle.vteximg.com.br/arquivos/ids/308906-530-706/HOMBRE-CAMISA-10099868-AZUL-740_1.jpg?v=637456368079070000',
+            'price' => 30000
+          ]);
     }
 }
